eadc5397d6315344fadc9061260c03e2be7d1414
Lucas Ramos <lucas.ramos.guimaraes@gmail.com>
2025-01-30 19:00:06 -0300

    script de buscar a api feita

---
+++ spotify-imersao-alura/script.js
@@ -0,0 +1,23 @@
+const searchInput = document.getElementById('search-input');
+const resultsArtists = document.getElementById('result-artist');
+const resultPlaylist = document.getElementById('result-playlist');
+
+function requestApi(searchTerm) {
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    fetch(url)
+        .then((response) => response.json())
+        // .then((result) => displayResults(result))
+}
+
+
+
+document.addEventListener('input', function () {
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === ''){
+        resultPlaylist.classList.add('hidden');
+    resultsArtists.classList.remove('hidden');
+    return;
+    }
+    
+    requestApi(searchTerm);
+});
